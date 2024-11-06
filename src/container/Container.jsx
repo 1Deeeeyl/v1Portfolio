ab9779ff3c53db9061cfccd42526914be9bfe5c4
randale-porciuncula <onedaleporciuncula@gmail.com>
2024-11-06 23:16:34 +0800

    Added components directory and container directory

---
+++ src/container/Container.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+function Container({children}) {
+  return (
+    <div className="text-blue-600">
+      {children}
+    </div>
+  )
+}
+
+
+export default Container
