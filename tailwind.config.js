814d5c0cb98f9ecf5026498bd59fa372a96ab8ca
randale-porciuncula <onedaleporciuncula@gmail.com>
2024-11-07 11:48:50 +0800

    Fixed font style and spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["DM Sans", "sans-serif"], // Default for sans-serif text
+      },fontWeight: {
+        light: 200,
+        normal: 400,
+        medium: 500,
+        bold: 700,
+      },
+    },
   },
   plugins: [],
-}
-
+};
