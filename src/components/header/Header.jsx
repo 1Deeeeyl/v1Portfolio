13b67d23c307bfb663b79a2ef20149b5244e304b
randale-porciuncula <onedaleporciuncula@gmail.com>
2024-11-07 14:00:05 +0800

    added basic navigation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,36 @@
 // import React from 'react'
-import "./Header.css"
+import "./Header.css";
+import Nav from "../nav/Nav";
 
 function Header() {
   return (
-    <header>
-      <h1 className="flex items-center font-sans font-bold">Hello, I&apos;m Dale <span className="ml-4"><img className="size-11" src="src\assets\images\emoji\wavingHand.png" alt="Waving emoji" /></span></h1>
-      <h2 className="mt-1 font-sans font-medium tracking-wider">Frontend Developer</h2>
-      <p className="flex items-center mt-5 font-sans font-medium tracking-wider"><img className="mr-1.5 size-5" src="src\assets\images\emoji\pin.png" alt="Pin emoji" />Philippines</p>
-      <p className="flex items-center font-sans font-medium tracking-wider"><img className="mr-1.5 size-5" src="src\assets\images\emoji\envelope.png" alt="Envelope Emoji" />porciuncula.randale@gmail.com</p>
+    <header >
+      <h1 className="font-sans font-bold ">
+        Hello, I&apos;m Dale
+        <span className="inline-block ml-2 align-center hand size-9"></span>
+      </h1>
+      <h2 className="mt-1 font-sans font-medium tracking-wider">
+        Frontend Developer
+      </h2>
+      <p className="flex items-center mt-5 font-sans font-medium tracking-wider">
+        <img
+          className="mr-1.5 size-5"
+          src="src\assets\images\emoji\pin.png"
+          alt="Pin emoji"
+        />
+        Philippines
+      </p>
+      <p className="flex items-center font-sans font-medium tracking-wider">
+        <img
+          className="mr-1.5 size-5"
+          src="src\assets\images\emoji\envelope.png"
+          alt="Envelope Emoji"
+        />
+        porciuncula.randale@gmail.com
+      </p>
+      <Nav></Nav>
     </header>
-  )
+  );
 }
 
-export default Header
-
-// ayusin font style
+export default Header;
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,30 @@
+// import React from 'react'
+
+function Nav() {
+  return (
+    <nav className="mt-12">
+      <ul>
+        <li className="flex items-center">
+          <span className="inline-block mr-2 bg-slate-50/40 size-1"></span>
+          <span className="font-sans text-base font-medium text-slate-50/40">
+            ABOUT ME
+          </span>
+        </li>
+        <li className="flex items-center">
+          <span className="inline-block mr-2 bg-slate-50/40 size-1"></span>
+          <span className="font-sans text-base font-medium text-slate-50/40">
+            EXPERIENCES
+          </span>
+        </li>
+        <li className="flex items-center">
+          <span className="inline-block mr-2 bg-slate-50/40 size-1"></span>
+          <span className="font-sans text-base font-medium text-slate-50/40">
+            PROJECTS
+          </span>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
