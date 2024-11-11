4a6014922bb79b85b9597f2b104acef0de0f66e7
randale-porciuncula <onedaleporciuncula@gmail.com>
2024-11-11 17:51:50 +0800

    added card template

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -1,4 +1,5 @@
 // import React from 'react'
+import Technologies from "../Technologies/Technologies";
 import "./AboutMe.css"
 
 function AboutMe() {
@@ -16,67 +17,10 @@ function AboutMe() {
         </p>
       </section>
       <section>
-        <h3 className="font-sans font-bold category">
+        <h2 className="font-sans font-bold category mb-[25px]">
           Technologies I Work With
-        </h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
-          voluptas asperiores quia esse autem, accusamus ex vitae ipsam rem
-          eveniet suscipit quo porro amet molestiae quos enim ipsum voluptatum
-          vel. Minus quisquam ab dolore minima alias nam repellendus fugit
-          recusandae facere, aspernatur reiciendis eveniet odit aperiam
-          repudiandae qui. Nam tempore necessitatibus temporibus, ea numquam,
-          inventore magni quaerat sit ad exercitationem accusantium atque
-          corporis nesciunt molestiae sapiente laborum cum ducimus. Odio sit est
-          dicta laborum, voluptatem alias perspiciatis impedit assumenda neque
-          cum, magni nihil nesciunt! Eaque excepturi dolorum necessitatibus
-          fugit! Sapiente, autem animi reprehenderit ipsum, excepturi eum sit
-          quibusdam temporibus quos incidunt impedit libero non, provident
-          suscipit corporis dolorum. Maxime dicta provident odio, harum non
-          magnam facere tempora, optio fugiat aut cupiditate corrupti quasi? Qui
-          illum nesciunt amet harum quod sit ipsam doloribus reprehenderit cum
-          ad quas eius itaque autem at dolor reiciendis sapiente inventore porro
-          quia error, in eligendi. Provident, maiores accusamus, repellat
-          recusandae blanditiis deleniti eligendi sunt amet ipsam fugit, ad modi
-          molestias quod laboriosam. Corrupti, deleniti! Ex voluptas sunt quia
-          accusantium neque consectetur. Repellendus molestiae fuga quam
-          laboriosam natus nihil ab modi cupiditate ut aperiam est, velit ipsam
-          architecto assumenda ratione officiis voluptatibus asperiores, quae
-          obcaecati ad eligendi facilis? Ducimus magni enim corrupti similique,
-          ut recusandae. Aliquid fuga dolores voluptate explicabo illum eos sed
-          rerum iusto ea est delectus quasi assumenda incidunt id molestias
-          soluta rem sint ad, et commodi sunt impedit possimus magni? Molestias
-          nam consectetur sapiente quisquam! Unde mollitia quisquam laudantium
-          esse a impedit? Ipsum, sint suscipit. Dolor pariatur, mollitia aliquid
-          ea iste incidunt accusamus, exercitationem error doloremque placeat
-          tempore neque optio perspiciatis a ex voluptatum delectus ullam
-          eveniet consectetur ab veniam aliquam nobis eius? Perferendis
-          necessitatibus corporis, dignissimos nam vero ullam saepe, laborum hic
-          autem ab amet deserunt. Culpa rem ducimus natus atque tempore.
-          Accusantium debitis eius quae nesciunt tempore velit dolor id, ratione
-          quisquam voluptatem praesentium quas labore. Eius omnis et expedita
-          ad. Ut tempora voluptatum nostrum soluta reprehenderit, exercitationem
-          explicabo. Quaerat rerum tempore cupiditate temporibus sit inventore,
-          eos illo velit necessitatibus aut magni quia cumque totam numquam
-          sequi, quibusdam ratione. Qui quia vero corporis nisi ducimus. Ex
-          error totam sunt, porro, facilis fugit, ea doloremque architecto
-          perspiciatis laudantium non perferendis aspernatur cumque nihil
-          dignissimos eius nostrum officia iure. Consectetur voluptatibus
-          explicabo maxime commodi, tenetur laboriosam quisquam totam recusandae
-          quas! Culpa, dolore qui voluptatem aliquam alias dolorum architecto
-          distinctio quos aut exercitationem. Ea labore totam accusamus
-          dignissimos nam, soluta iusto quisquam consectetur, laborum, et optio
-          quo omnis dolor? Quae dolorem aspernatur laboriosam ab soluta itaque
-          esse nobis sed sequi? Quae quis tenetur delectus amet dolorum
-          reiciendis laboriosam libero id quam iure, minus porro ut temporibus
-          omnis labore perferendis illum. Sed quisquam iure ut sapiente placeat
-          obcaecati recusandae hic dicta quis numquam dolore ipsam ipsa repellat
-          praesentium temporibus aliquam voluptas vitae, suscipit tempora
-          eligendi? Sint nisi ipsam optio totam ad. Unde, reiciendis ad. Quae
-          quod laudantium ad obcaecati placeat beatae velit dolor officiis
-          adipisci, voluptatem optio corrupti accusantium sunt amet in, qui ex
-          dolores esse.
-        </p>
+        </h2>
+        <Technologies></Technologies>
       </section>
     </section>
   );
