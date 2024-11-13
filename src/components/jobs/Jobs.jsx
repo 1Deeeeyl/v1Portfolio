35971abb1cca56068c05d0c7ee89d40da9d27a9b
randale-porciuncula <onedaleporciuncula@gmail.com>
2024-11-13 16:14:29 +0800

    experience section

--- src/components/jobs/Jobs.jsx
+++ src/components/jobs/Jobs.jsx
@@ -3,9 +3,27 @@
 function Jobs() {
   return (
     <li>
-        
+      <div className="">
+        <h3 className="font-sans text-xl font-medium tracking-wide mb-2.5">
+          Freelance Multimedia Designer @{" "}
+          <span className="font-sans font-bold text-[#48CAE4]">iPhiTech</span>
+        </h3>
+        <p className="text-slate-300 mb-2.5">August 2023 - November 2023</p>
+        <div className="flex">
+          <span className="inline-block mr-2 bg-slate-50/40 size-1 min-w-fit"></span>
+          <p className="font-sans font-normal leading-relaxed tracking-wider text-slate-300 ">
+            At iPhiTech IT and Digital Solutions, I worked as a freelance
+            Multimedia Designer, focusing on designing user-friendly websites
+            and mobile app interfaces that aligned with clients’ brand
+            identities. I created graphics, edited videos, and developed product
+            mockups to help brands increase engagement and improve their
+            marketing efforts.
+          </p>
+        </div>
+      </div>
     </li>
-  )
+  );
 }
 
-export default Jobs
+export default Jobs;
+// bullet
