fb52b373228159630557aea05eea696965773de2
randale-porciuncula <onedaleporciuncula@gmail.com>
2024-11-14 14:49:06 +0800

    experience section done

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,14 +1,25 @@
 // import React from 'react'
 
 import Jobs from "../jobs/Jobs";
+import items from "../../json/experience.json";
 
 function Experience() {
   return (
     <section>
       <section className="mb-[25px]">
         <h2 className="font-sans font-bold  mb-[25px] category">Experience</h2>
-        <ul>
-          <Jobs />
+        <ul className="flex flex-col gap-[25px]">
+          {items.map((item, index) => {
+            return (
+              <Jobs
+                key={index}
+                position={item.position}
+                company={item.company}
+                date={item.date}
+                description={item.description}
+              />
+            );
+          })}
         </ul>
       </section>
     </section>
