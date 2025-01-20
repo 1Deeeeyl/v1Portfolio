7a7ae521870e408da63b279ed2a87a361cfaedb4
randale-porciuncula <onedaleporciuncula@gmail.com>
2025-01-20 18:38:54 +0800

    update

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -1,6 +1,6 @@
 // import React from 'react'
-import Technologies from "../technologies/Technologies";
-import "./AboutMe.css";
+import Technologies from '../technologies/Technologies';
+import './AboutMe.css';
 import { Element } from 'react-scroll';
 function AboutMe() {
   return (
@@ -8,14 +8,32 @@ function AboutMe() {
       <h2 className="sticky top-0 z-20 w-screen px-6 py-[25px] font-sans font-bold  lg:mb-[25px] category -mx-6 backdrop-blur md:-mx-12 md:px-12  lg:static lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0">
         About Me
       </h2>
-     
 
       <section>
         <p className="font-sans font-normal leading-relaxed tracking-wider text-slate-300 mb-[25px]">
-          I<span>&apos;</span>m a 23-year-old <span className="font-bold hover:text-[#25DBFF] transition-all ease-in-out duration-200">former UI/UX designer turned
-          frontend developer</span>, now diving into backend development to become a
-          full-stack developer. Away from the keyboard, I spend my time learning
-          how to draw, working under my car, or taking photos outside.
+          I<span>&apos;</span>m a 23-year-old&nbsp;
+          <span className="font-bold hover:text-[#25DBFF] transition-all ease-in-out duration-200">
+            former UI/UX designer turned front-end developer
+          </span>
+          . In my free time, I study back end development to expand my skill set
+          and work towards becoming a full-stack developer, capable of handling
+          end-to-end solutions.
+        </p>
+
+        <p className="font-sans font-normal leading-relaxed tracking-wider text-slate-300 mb-[25px]">
+          Away from the keyboard, I like to{' '}
+          <span className="font-bold hover:text-[#25DBFF] transition-all ease-in-out duration-200">
+            sketch for fun
+          </span>
+          ,{' '}
+          <span className="font-bold hover:text-[#25DBFF] transition-all ease-in-out duration-200">
+            tinker with my car
+          </span>
+          , or{' '}
+          <span className="font-bold hover:text-[#25DBFF] transition-all ease-in-out duration-200">
+            head outside with my camera
+          </span>{' '}
+          to capture whatever catches my eye.
         </p>
 
         <h2 className="font-sans font-bold category mb-[25px]">
@@ -23,9 +41,9 @@ function AboutMe() {
         </h2>
         <Technologies />
         <p className="font-sans text-[0.8rem] mt-3">
-          I am currently learning <span className="font-bold">Node.JS</span>,{" "}
-          <span className="font-bold">Express</span>, and{" "}
-          <span className="font-bold">MongoDB</span>.
+          I am currently learning <span className="font-bold hover:text-[#25DBFF] transition-all ease-in-out duration-200">Node.JS</span>,{' '}
+          <span className="font-bold hover:text-[#25DBFF] transition-all ease-in-out duration-200">Express</span>, and{' '}
+          <span className="font-bold hover:text-[#25DBFF] transition-all ease-in-out duration-200">MongoDB</span>.
         </p>
       </section>
     </Element>
